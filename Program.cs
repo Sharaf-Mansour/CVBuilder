a12fc89cf53810da4a81c0c3b063372ade53d728
Sharaf M. Mansour <55330747+Sharaf-Mansour@users.noreply.github.com>
2021-10-03 00:11:03 +0200

    Download CV Raw Data

--- Program.cs
+++ Program.cs
@@ -4,5 +4,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => 
 new HttpClient { BaseAddress = 
 new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<CVModel>();
+// builder.Services.AddScoped<CVModel>();
 await builder.Build().RunAsync();
