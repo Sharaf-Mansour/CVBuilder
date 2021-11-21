e3a50676cbb3e23bd37f8c0c45027b351630daff
Sharaf M. Mansour <55330747+Sharaf-Mansour@users.noreply.github.com>
2021-11-21 03:34:02 +0200

    Fix #2

--- wwwroot/service-worker.published.js
+++ wwwroot/service-worker.published.js
@@ -1,10 +1,6 @@
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
-
 self.importScripts('./service-worker-assets.js');
-
-
-
 
 async function onFetch(event) {
     let cachedResponse = null;
@@ -20,10 +16,6 @@ async function onFetch(event) {
     return cachedResponse || fetch(event.request);
 }
 
-
-
-
-
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
 self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
 self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
@@ -34,18 +26,17 @@ const offlineAssetsExclude = [ /^service-worker\.js$/ ];
 
 async function onInstall(event) {
     console.info('Service worker: Install');
-
     // Fetch and cache all matching items from the assets manifest
     const assetsRequests = self.assetsManifest.assets
         .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
         .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
         .map(asset => new Request(asset.url, { integrity: asset.hash, cache: 'no-cache' }));
     await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
+    self.skipWaiting();
 }
 
 async function onActivate(event) {
     console.info('Service worker: Activate');
-
     // Delete unused caches
     const cacheKeys = await caches.keys();
     await Promise.all(cacheKeys
