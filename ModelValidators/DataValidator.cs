1d28ba1cd9367112ff45cbca5214f8059db4f099
Sharaf M. Mansour <55330747+Sharaf-Mansour@users.noreply.github.com>
2021-11-25 04:43:15 +0200

    Validator Improve

--- ModelValidators/DataValidator.cs
+++ ModelValidators/DataValidator.cs
@@ -5,7 +5,7 @@ public class DataValidator : AbstractValidator<Data>
     {
         RuleFor(x => x.Value)
             .NotEmpty().WithMessage("Please enter Data")
-            .MaximumLength(70).WithMessage("Maxmum Length is 70 letter");
+            .MaximumLength(100).WithMessage("Maximum Length is 100 letter");
         RuleFor(x => x.Link)
             .Must(uri => Uri.TryCreate(uri, UriKind.Absolute, out _))
             .When(x => x.IsLink)
