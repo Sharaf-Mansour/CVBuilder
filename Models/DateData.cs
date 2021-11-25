5e0f6a93e48283031f4078d508028c6aee615ca3
Sharaf M. Mansour <55330747+Sharaf-Mansour@users.noreply.github.com>
2021-11-25 03:42:18 +0200

    Move Static to Imports

--- Models/DateData.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace CVBuilder.Models;
-public static class DateData
-{
-    public static Span<int> Years() => Enumerable.Range(1900, 2023 - 1900).Reverse().ToArray();
-    public static Span<Months> Month() => (Span<Months>)Enum.GetValues(typeof(Months))!;
-}
