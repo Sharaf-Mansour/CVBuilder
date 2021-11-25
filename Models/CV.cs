6127b6cf5a139f97a599c7bcdccfb34bbd7d58e0
Sharaf M. Mansour <55330747+Sharaf-Mansour@users.noreply.github.com>
2021-11-25 06:46:12 +0200

    Add Validation level on CV Model

--- Models/CV.cs
+++ Models/CV.cs
@@ -1,20 +1,14 @@
 ﻿namespace CVBuilder.Models;
-
-public class CV {
+public class CV: IValidation {
   public CVType CVtype { get; set; }
     public PersonalDataModel PersonalData { get; set; } = new ();
-
     public List<Education> ? Educations { get; set; } = new ();
-
     public List<Experience> ? WorkExperience { get; set; } = new ();
-
     public List<Experience> ? OtherExperience { get; set; } = new ();
-
     public List<Data> ? Certifications { get; set; } = new ();
-
     public List<Data> ? ProfessionalAffeilations { get; set; } = new ();
-
     public List<Data>? OpenSource { get; set; } = new();
-
     public List<References > ? Refernces { get; set; } = new ();
+    public bool InEditState { get; set; }
+    public bool IsValid() => new CVValidator().Validate(this).IsValid;
 }
