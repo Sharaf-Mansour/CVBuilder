314fd985c2812d95abba2eb1b9d05245eb307ef8
Sharaf M. Mansour <55330747+Sharaf-Mansour@users.noreply.github.com>
2021-11-30 09:24:47 +0200

    #3 Fix

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 await builder.Build().RunAsync();
---
+++ Shared/AddressInput.razor.cs
@@ -0,0 +1,13 @@
+namespace CVBuilder.Shared;
+public partial class AddressInput
+{
+    private CountryState[]? CountryStates;
+     public StateProvince[]? States(int? ID) => CountryStates?.FirstOrDefault(x => x.Country.Id == ID)?.StateProvince;
+   [Parameter]  public AddressModel? Address { get; set; }
+    public int? StateId { get => Address?.State?.Id; set => ( Address!.State) = ( States(Address?.Country?.Id)?.FirstOrDefault(x=> x.Id == value)); }
+    public int? CountryId { get => Address?.Country?.Id; set => (StateId, Address!.Country) = (null, CountryStates?.FirstOrDefault(x=>x.Country.Id == value)?.Country); }
+    [Parameter] public string? Id { get; set; }
+    protected override async Task OnInitializedAsync() => CountryStates = await Http.GetFromJsonAsync<CountryState[]>("CountryStateArray.json");
+  //  string Valid => Address!.Country is null ? "is-invalid form-control" : "is-valid ";
+
+}
