15c36bd0ab422a02a670e1400ccb99b4d1e78a4f
Sharaf M. Mansour <55330747+Sharaf-Mansour@users.noreply.github.com>
2021-12-20 05:30:02 +0200

    Clean up date

--- Models/PersonalDataModel.cs
+++ Models/PersonalDataModel.cs
@@ -1,10 +1,8 @@
 ﻿namespace CVBuilder.Models;
-public class PersonalDataModel : IValidation
+public class PersonalDataModel
 {
-    public string? Name {  get; set; }
-    public string? Email {  get; set; }
-     public string? Phone {  get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
     public AddressModel Address { get; set; } = new();
-    public bool InEditState { get; set; }
-    public virtual bool IsValid() => new PersonValidator().Validate(this).IsValid;
 }
