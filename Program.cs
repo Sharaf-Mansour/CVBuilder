6858b7a58a4b2a200d9a9796d62c0f532fc50f66
Sharaf M. Mansour <55330747+Sharaf-Mansour@users.noreply.github.com>
2021-12-20 05:29:36 +0200

    Fix #32

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+HttpClient HttpClient() => new() { BaseAddress = new(builder.HostEnvironment.BaseAddress) };
+DateData.CountryStates = await HttpClient().GetFromJsonAsync<CountryState[]>("CountryStateArray.json") ?? throw new();
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 await builder.Build().RunAsync();
