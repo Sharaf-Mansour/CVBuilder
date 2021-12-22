d18fc3982e03ddd9ed7ff6000c752b20bb64a7b4
Sharaf M. Mansour <55330747+Sharaf-Mansour@users.noreply.github.com>
2021-12-22 07:26:42 +0200

    Code Clean up

--- Models/Country.cs
+++ Models/Country.cs
@@ -2,8 +2,8 @@
 public class Country
 {
     private int id;
-    private Country? _(int value) => DateData.CountryStates?.FirstOrDefault(x => x.Country.Id == value)?.Country;
-    public int Id { get => id; set => (id, Name, TwoLetterIsoCode, ThreeLetterIsoCode) = (value, _(value)?.Name, _(value)?.TwoLetterIsoCode, _(value)?.ThreeLetterIsoCode); }
+    private static Country? SetCountry(int value) => DateData.CountryStates?.FirstOrDefault(x => x.Country.Id == value)?.Country;
+    public int Id { get => id; set => (id, Name, TwoLetterIsoCode, ThreeLetterIsoCode) = (value, SetCountry(value)?.Name, SetCountry(value)?.TwoLetterIsoCode, SetCountry(value)?.ThreeLetterIsoCode); }
     public string? Name { get; set; }
     public string? TwoLetterIsoCode { get; set; }
     public string? ThreeLetterIsoCode { get; set; }
