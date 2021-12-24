3620623800aa297227f95169415f4d918f57b524
Sharaf M. Mansour <55330747+Sharaf-Mansour@users.noreply.github.com>
2021-12-24 18:40:50 +0200

    HotFix for Nav

--- Service/UiUpdate.cs
+++ Service/UiUpdate.cs
@@ -4,4 +4,5 @@ public static class UiUpdate
     public static event Action? OnChange;
     public static void NotifyStateChanged() => OnChange?.Invoke();
     public static Tabs SelectedTab { get; set; } = Tabs.HomePage;
+    public static int SelectedTabIndex { get; set; } = 0;
 }
