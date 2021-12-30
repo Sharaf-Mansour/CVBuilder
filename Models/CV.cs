5c24c993b7b201d800570a4042a2102d8e229bc0
Sharaf M. Mansour <55330747+Sharaf-Mansour@users.noreply.github.com>
2021-12-30 20:22:42 +0200

    Fix #39

--- Models/CV.cs
+++ Models/CV.cs
@@ -9,6 +9,7 @@ public class CV : IValidation
         new() { Tab = Tabs.Education, IsSelected = true },
         new() { Tab = Tabs.WorkExprince, IsSelected = true },
         new() { Tab = Tabs.OtherWorkExprince, IsSelected = true },
+        new() { Tab = Tabs.Projects, IsSelected = true },
         new() { Tab = Tabs.Certifications, IsSelected = true },
         new() { Tab = Tabs.ProfessionalAffeilations, IsSelected = true },
         new() { Tab = Tabs.Refernces, IsSelected = true },
@@ -18,9 +19,9 @@ public class CV : IValidation
     public List<Education>? Educations { get; set; } = new();
     public List<Experience>? WorkExperience { get; set; } = new();
     public List<Experience>? OtherExperience { get; set; } = new();
+    public List<DataLink>? Projects { get; set; } = new();
     public List<Certificate>? Certifications { get; set; } = new();
     public List<Data>? ProfessionalAffeilations { get; set; } = new();
-    public List<Data>? OpenSource { get; set; } = new();
     public List<References>? Refernces { get; set; } = new();
     public bool InEditState { get; set; }
     public bool IsValid() => new CVValidator().Validate(this).IsValid;
