b7109cfae9739aa9b231274dd7274a0ccafe2872
Sharaf M. Mansour <55330747+Sharaf-Mansour@users.noreply.github.com>
2022-02-13 03:54:54 +0200

    fixed name for workplace

--- ModelValidators/EducationValidator.cs
+++ ModelValidators/EducationValidator.cs
@@ -4,7 +4,7 @@ public class EducationValidator : AbstractValidator<Education>
     public EducationValidator()
     {
         RuleFor(x => x.LocationName)
-            .NotEmpty().WithMessage("School Name required.")
+            .NotEmpty().WithMessage("Name is required.")
             .MaximumLength(50).WithMessage("Maximum Length is 50 letter");
         RuleForEach(x => x.Data).SetValidator(new DataValidator());
         RuleFor(x => x.Date).SetValidator(new DateValidator());
--- ModelValidators/ExperienceValidator.cs
+++ ModelValidators/ExperienceValidator.cs
@@ -4,12 +4,12 @@ public class ExperienceValidator : AbstractValidator<Experience>
     public ExperienceValidator()
     {
         RuleFor(x => x.LocationName)
-            .NotEmpty().WithMessage("Work Place Name required.")
-            .MaximumLength(50).WithMessage("Maximum Length is 50 letter");
+        .NotEmpty().WithMessage("Name is required.")
+        .MaximumLength(50).WithMessage("Maximum Length is 50 letter");
+        RuleForEach(x => x.Data).SetValidator(new DataValidator());
+        RuleFor(x => x.Date).SetValidator(new DateValidator());
         RuleFor(x => x.JobTitle)
             .NotEmpty().WithMessage("Job Title required.")
             .MaximumLength(50).WithMessage("Maximum Length is 50 letter");
-        RuleForEach(x => x.Data).SetValidator(new DataValidator());
-        RuleFor(x => x.Date).SetValidator(new DateValidator());
     }
 }
