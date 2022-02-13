2a3673f998f78982b7df742c3c5988ab8c73da70
Sharaf M. Mansour <55330747+Sharaf-Mansour@users.noreply.github.com>
2022-02-13 03:54:13 +0200

    Fix typo

--- Models/Education.cs
+++ Models/Education.cs
@@ -1,11 +1,11 @@
 ﻿namespace CVBuilder.Models;
 public class Education : ICloneable, IValidation
 {
-    public  string? LocationName { get; set; }
+    public string? LocationName { get; set; }
     public List<Data>? Data { get; set; } = new();
     //public bool IsValid() => !string.IsNullOrWhiteSpace(LocationName) && !string.IsNullOrWhiteSpace(Year);
     public virtual bool IsValid() => new EducationValidator().Validate(this).IsValid;
     public bool InEditState { get; set; }
     object ICloneable.Clone() => this.DeepClone();
-    public DateModel Date { get; set; } = new(); 
+    public DateModel Date { get; set; } = new();
 }
--- Service/Tabs.cs
+++ Service/Tabs.cs
@@ -1,5 +1,5 @@
 ﻿namespace CVBuilder;
 public enum Tabs
 {
-    HomePage, Personal, Education, WorkExprince, OtherWorkExprince, Projects, Certifications, ProfessionalAffiliations, Refernces, CV, Null
+    HomePage, Personal, Education, WorkExperience, OtherWorkExperience, Projects, Certifications, ProfessionalAffiliations, Refernces, CV, Null
 }
