69ffa51469536b5eff33edaedcfc20adf5b3cc6f
Sharaf-Mansour <sharaf_1015@yahoo.com>
2022-07-29 20:06:26 +0200

    Improve skills section

---
+++ ModelValidators/SkillValidator.cs
@@ -0,0 +1,12 @@
+﻿namespace CVBuilder.ModelsValidators;
+
+public class SkillValidator : AbstractValidator<Skill>
+{
+    public SkillValidator()
+    {
+        RuleFor(x => x.SkillType)
+            .NotEmpty().WithMessage("Skill Type is required.")
+            .MaximumLength(50).WithMessage("Maximum Length is 50 letter");
+        RuleForEach(x => x.Data).SetValidator(new DataValidator());
+    }
+}
---
+++ Models/Skill.cs
@@ -0,0 +1,9 @@
+﻿namespace CVBuilder.Models;
+
+public class Skill : IValidation
+{
+    public string? SkillType { get; set; }
+    public List<Data>? Data { get; set; } = new();
+    public bool InEditState { get; set; }
+    public bool IsValid() => new SkillValidator().Validate(this).IsValid;
+}
