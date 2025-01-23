4e2591550b19aec46fe0daea3a472f4cafd507f8
Sharaf M. Mansour <55330747+Sharaf-Mansour@users.noreply.github.com>
2025-01-23 21:57:31 +0200

    CODERUB

--- Shared/InputSummery.razor.cs
+++ Shared/InputSummery.razor.cs
@@ -1,8 +1,8 @@
 namespace CVBuilder.Shared;
 public partial class InputSummery
 {
-    string[]? rows => PersonalDataModel?.Summary?.Split(Environment.NewLine.ToCharArray());
-    int? maxLength => (rows?.Max(x => x.Length) > 100 || rows?.Length > 20 || PersonalDataModel?.Summary?.Length > 2000 ? PersonalDataModel?.Summary?.Length : 2000) ?? 0;
+    string[]? Rows => PersonalDataModel?.Summary?.Split(Environment.NewLine.ToCharArray());
+    int? MaxLength => (Rows?.Max(x => x.Length) > 100 || Rows?.Length > 20 || PersonalDataModel?.Summary?.Length > 2000 ? PersonalDataModel?.Summary?.Length : 2000) ?? 0;
     [Parameter] public PersonalDataModel? PersonalDataModel { get; set; }
     [Parameter] public string? Id { get; set; }
 }
