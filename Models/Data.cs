0b3997450f7e0efcefe04e26bf6596768dcfa17d
Sharaf M. Mansour <55330747+Sharaf-Mansour@users.noreply.github.com>
2025-10-28 13:28:22 +0300

    CODERUB

--- Models/Data.cs
+++ Models/Data.cs
@@ -1,9 +1,8 @@
 ﻿namespace CVBuilder.Models;
 public class Data : IValidation
 {
-    private bool isLink;
     public string? Value { get; set; }
-    public bool IsLink { get => isLink; set => (isLink, Link) = (value, value ? Link : null); }
+    public bool IsLink { get => field; set => (field, Link) = (value, value ? Link : null); }
     public string? Link { get; set; }
     public virtual bool IsValid() => new DataValidator().Validate(this).IsValid;
     public bool InEditState { get; set; }
--- Models/Education.cs
+++ Models/Education.cs
@@ -2,7 +2,7 @@
 public class Education : IValidation
 {
     public string? LocationName { get; set; }
-    public List<Data>? Data { get; set; } = new();
+    public List<Data>? Data { get; set; } = [];
     public virtual bool IsValid() => new EducationValidator().Validate(this).IsValid;
     public bool InEditState { get; set; }
     public DateModel Date { get; set; } = new();
